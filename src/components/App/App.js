d98bfd7a2f00c33271203d164734376b85c14c63
Nikita Trofimov <nikita.igorevi4@yandex.ru>
2021-07-21 12:52:29 +0300

    сверстан Promo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,16 @@
 import logo from '../../images/logo.svg';
 import './App.css';
 import Header from '../Header/Header';
+import Main from '../Main/Main';
+import Promo from '../Promo/Promo';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main>
+        <Promo />
+      </Main>
     </div>
   );
 }
---
+++ src/components/Promo/Promo.js
@@ -0,0 +1,13 @@
+import './Promo.css';
+// import backgound from '../../images/text__COLOR_landing-logo.svg'
+
+function Promo() {
+  return (
+    <section className="promo">
+      <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+      <div className="promo__background-img"></div>
+    </section>
+  )
+}
+
+export default Promo;

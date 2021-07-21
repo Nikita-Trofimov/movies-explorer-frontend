65732aa6ac49619f334273df6928af4a12b0c43e
Nikita Trofimov <nikita.igorevi4@yandex.ru>
2021-07-21 11:18:13 +0300

    сверстан Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import './Header.css';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header className="header">
+      <Link to='/' className="header__logo"/>
+      <nav className="header__authentication">
+        <Link to='/signup' className="header__authentication-link">Регистрация</Link>
+        <Link to='/signin' className="header__authentication-link header__authentication-button">Войти</Link>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;

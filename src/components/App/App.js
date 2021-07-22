51004c0a0013c502f45b0a65498ba6969d55e76a
Nikita Trofimov <nikita.igorevi4@yandex.ru>
2021-07-22 11:51:36 +0300

    сделал адаптивный AboutProject

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,8 +14,8 @@ function App() {
       <Header />
       <Main>
         <Promo />
-        {/* <AboutProject />
-        <Techs />
+        <AboutProject />
+        {/* <Techs />
         <AboutMe />
         <Portfolio />
         <Footer /> */}
